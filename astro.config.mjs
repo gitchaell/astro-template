62f9468f9002f5c350eb05ce0c334e11a65e7f55
gitchaell <f.michaell.a.m@gmail.com>
2025-03-15 17:10:49 -0400

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://amazing.app',
+	integrations: [sitemap({
+		i18n: {
+			defaultLocale: 'en',
+			locales: {
+				en: 'en-US',
+				es: 'es-ES',
+				de: 'de-DE',
+				ja: 'ja-JP',
+				ko: 'ko-KR',
+			},
+		},
+	})],
+
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
 	i18n: {
 		locales: ['en', 'es', 'de', 'ja', 'ko'],
 		defaultLocale: 'en',
