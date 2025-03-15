5999e91e8d5428e9ed7c3690bc8114cc5e97fd62
gitchaell <f.michaell.a.m@gmail.com>
2025-03-15 17:24:49 -0400

    chore: setup a11y

--- eslint.config.js
+++ eslint.config.js
@@ -2,18 +2,20 @@ import globals from 'globals'
 import jslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import astrolint from 'eslint-plugin-astro'
+import a11ylint from 'eslint-plugin-jsx-a11y'
 import prettierlint from 'eslint-config-prettier'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	{
 		files: ['**/*.{js,mjs,cjs,ts}'],
-		ignores: ['**/node_modules/**', '**/dist/**', '**/build/**', '**/.astro/**'],
+		ignores: ['/node_modules/**', '/dist/**', '/.astro/**'],
 	},
 	{
 		languageOptions: {
 			globals: {
 				...globals.browser,
+				...globals.serviceworker,
 				...globals.node,
 			},
 		},
@@ -21,6 +23,7 @@ export default [
 	jslint.configs.recommended,
 	...tseslint.configs.recommended,
 	...astrolint.configs.recommended,
+	a11ylint.flatConfigs.recommended,
 	prettierlint,
 	{
 		rules: {
