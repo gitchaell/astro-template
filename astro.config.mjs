e1bc161d24bcf1f761202302e5cd2585b797ff16
gitchaell <f.michaell.a.m@gmail.com>
2025-03-15 18:13:14 -0400

    chore: setup PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,51 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import sitemap from '@astrojs/sitemap';
+import AstroPWA from '@vite-pwa/astro'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://amazing.app',
-	integrations: [sitemap({
-		i18n: {
-			defaultLocale: 'en',
-			locales: {
-				en: 'en-US',
-				es: 'es-ES',
-				de: 'de-DE',
-				ja: 'ja-JP',
-				ko: 'ko-KR',
+	integrations: [
+		sitemap({
+			i18n: {
+				defaultLocale: 'en',
+				locales: {
+					en: 'en-US',
+					es: 'es-ES',
+					de: 'de-DE',
+					ja: 'ja-JP',
+					ko: 'ko-KR',
+				},
 			},
-		},
-	})],
+		}),
+		AstroPWA({
+			mode: 'development',
+			base: '/',
+			scope: '/',
+			includeAssets: ['favicon.svg'],
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'Astro Template',
+				short_name: 'Astro Template',
+				theme_color: '#000000',
+			},
+			pwaAssets: {
+				config: true,
+			},
+			workbox: {
+				navigateFallback: '/404',
+				globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+			},
+			devOptions: {
+				enabled: false,
+				navigateFallbackAllowlist: [/^\//],
+			},
+			experimental: {
+				directoryAndTrailingSlashHandler: true,
+			}
+		})
+	],
 
 	vite: {
 		plugins: [tailwindcss()],
